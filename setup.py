22d94a2da8d81bd8ed336bede37ebbec4101900e
Smart Hacker <sarthakmishra2100@gmail.com>
2024-10-14 20:53:32 +0100

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'SmartHackerTools',         # How you named your package folder (MyLib)
   packages = ['SmartHackerTools'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'These contain some smart hacker tools!',   # Give a short description about your library
   author = 'Smart_Hacker',                   # Type in your name
@@ -12,6 +12,8 @@ setup(
   keywords = ['Tools', 'Simple', 'Beginner'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'numpy',
+          'time',
+          'sys',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
