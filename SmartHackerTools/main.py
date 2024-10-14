bed2f81527747a064da48e4977918e0f58fcc419
Smart Hacker <sarthakmishra2100@gmail.com>
2024-10-14 20:52:13 +0100

    new file:   SmartHackerTools/__pycache__/main.cpython-311.pyc 	modified:   SmartHackerTools/main.py

--- SmartHackerTools/main.py
+++ SmartHackerTools/main.py
@@ -1,2 +1,29 @@
-def foo():
-  print("hello world")
+import time
+import sys
+
+def type_writer(text, delay=0.08, after=""):
+    """
+    Simulates typing on the console by printing the given text character by
+    character with a delay in between. The delay can be specified with the
+    delay argument. The default delay is 0.08 seconds.
+
+    Parameters:
+        text (str): The text to be typed
+        delay (float): The time in seconds to wait after each character
+        after (str): The text to print after the text has been typed
+    """
+    for char in text:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(delay)
+    print(after)
+
+def version():
+    """
+    Returns the current version of the SmartHackerTools.
+    
+    Returns:
+        str: The version of the SmartHackerTools
+    """
+    return "v_001"
+
